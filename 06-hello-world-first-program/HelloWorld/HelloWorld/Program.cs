ce64ced31024339e2bea5e9ac9661a038b9dffd4
jkrovitz <jeremy@jeremykrovitz.com>
2022-10-06 20:35:51 -0500

    Add code and notes from video 6
    Added code and notes from video 6. Hello World - First Program.

---
+++ 06-hello-world-first-program/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        // Entry point of our program
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Jeremy");
+            Console.Read();
+        }
+    }
+}
